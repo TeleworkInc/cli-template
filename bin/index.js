5903121716e458bfad625437f71d6965f74bcc2d
Christian Lewis <ctjl96@gmail.com>
2020-07-06 02:21:34 -0500

    BULK AUTOMATIC COMMIT: Mon 06 Jul 2020 02:21:34 AM CDT

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 
+/**
+ * Define actions.
+ */
+const sayHello = () => console.log('Hello world!');
+
+/**
+ * Define commands and assign actions to them.
+ */
 program
     .command('hello')
     .description('Say hello to the world.')
-    .action(() => console.log('Hello world!'));
+    .action(sayHello);
 
+/**
+ * Parse process arguments.
+ */
 program.parse(process.argv);
+
+/**
+ * Export anything that is needed for index.js.
+ */ 
+module.exports = sayHello;
