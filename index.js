a23550fb32d7d87bbeac539d67415f6a39da9e34
Christian Lewis <ctjl96@gmail.com>
2020-07-06 03:27:37 -0500

    BULK AUTOMATIC COMMIT: Mon 06 Jul 2020 03:27:37 AM CDT

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const chalk = require('chalk');
 /**
  * Slightly stylized logging utils. 
  */
-const log = (msg) => console.log(
+const log = (...msgs) => console.log(
     chalk.bgBlueBright(' MSG '),
-    msg
+    ...msgs
 );
 
-const success = (msg) => console.log(
-   chalk.bgGreen(' SUCCESS '),
-   msg
+const success = (...msgs) => console.log(
+    chalk.bgGreen(' SUCCESS '),
+    ...msgs
 );
 
-const error = (msg) => console.log(
-   chalk.bgRed(' ERROR '),
-   msg
+const error = (...msgs) => console.log(
+    chalk.bgRed(' ERROR '),
+    ...msgs
 );
 
 /**
