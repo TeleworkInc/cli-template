c4604d00efac925052372ca7432aabc257055800
Christian Lewis <ctjl96@gmail.com>
2020-07-06 02:31:01 -0500

    BULK AUTOMATIC COMMIT: Mon 06 Jul 2020 02:31:01 AM CDT

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /**
- * Define and export any core logic here.
+ * Define any core logic here.
  */
 const sayHello = () => console.log('Hello world!');
 
+/**
+ * Specify exports for use in bin file and when
+ * this package is `require`d.
+ */
 module.exports = {
     sayHello,
 };
